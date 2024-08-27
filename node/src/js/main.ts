39a64d75462b332fef352768403c2e2726fbfde0
yukimasaki <woodenhoe@gmail.com>
2024-08-27 16:21:44 +0900

    feat: 自作テーマで使用中のcss、jsをsrcディレクトリにコピー

---
+++ node/src/js/main.ts
@@ -0,0 +1,7 @@
+const toggleMenu = () => {
+  const navigation = document.querySelector(".__navigation");
+  navigation?.classList.toggle("__open");
+
+  const hamburgerIcon = document.querySelector(".__hamburgerIcon");
+  hamburgerIcon?.classList.toggle("__open");
+};
